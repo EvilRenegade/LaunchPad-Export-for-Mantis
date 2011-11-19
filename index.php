328ad925da90c1b8109d4fb98d40d9161de38d5c
Renegade <renegade@renegadeprojects.com>
2011-11-19 13:20:04 +0100

    This should be feature complete; now to test it...

--- index.php
+++ index.php
@@ -2,7 +2,7 @@
 require_once("lpxmlwriter.php");
 header("Content-type: text/xml");
 
-$lp = new LpXmlWriter("php://output");
+$lp = new lpXmlWriter("php://output");
 
 $lp->startBug(42);
 $lp->writePerson();
@@ -10,4 +10,4 @@ $lp->endBug();
 
 unset($lp);
 
-LpXmlWriter::getResult();
+lpXmlWriter::getResult();
--- lpxmlwriter.php
+++ lpxmlwriter.php
@@ -1,7 +1,7 @@
 <?php
 // based on allenap's bug-export.rnc revision 14276
 
-class LpXmlWriter extends XMLWriter {
+class lpXmlWriter extends XMLWriter {
 	private $inMemory = false;
 	private $bugIsOpen = false;
 	private static $result = NULL;
@@ -28,6 +28,10 @@ class LpXmlWriter extends XMLWriter {
 	public static $LOW			= "LOW";
 	public static $WISHLIST		= "WISHLIST";
 	public static $UNDECIDED	= "UNDECIDED";
+	
+	// LaunchPad patch statuses
+	public static $PATCH		= "PATCH";
+	public static $UNSPECIFIED	= "UNSPECIFIED";
 
 	
 	// LaunchPad datatypes
@@ -51,6 +55,10 @@ class LpXmlWriter extends XMLWriter {
 		return $pName; // todo validate against rxp
 	}
 	
+	public static function getTimestamp($pTimestamp) {
+		return str_replace('+00:00', 'Z', gmdate('c', $pTimestamp));
+	}
+	
 	// constructor, takes path of output file
 	public function __construct($pFilePath) {
 		if(is_null($pFilePath)) {
@@ -63,7 +71,7 @@ class LpXmlWriter extends XMLWriter {
 		
 		$this->setIndent(true);
 		$this->startDocument("1.0");
-		$this->startElementNS(NULL, "launchpad-bugs", LpXmlWriter::$xmlNs);
+		$this->startElementNS(NULL, "launchpad-bugs", lpXmlWriter::$xmlNs);
 	}
 	
 	public function __destruct() {
@@ -72,11 +80,12 @@ class LpXmlWriter extends XMLWriter {
 			$this->debug("Closing the document even though a bug is still marked as open; please check your endBug()s are in balance with your startBug()s.");
 		}
 		$this->endDocument();
-		LpXmlWriter::$result = $this->flush();
+		lpXmlWriter::$result = $this->flush();
 	}
 	
 	public static function getResult() { // returns the result of XMLWriter::flush of the most recent destruction
-		return LpXmlWriter::$result;
+		//var_dump(lpXmlWriter::$result);
+		return lpXmlWriter::$result;
 	}
 	
 	public function writeNobody($pTagName = "person") {
@@ -91,7 +100,7 @@ class LpXmlWriter extends XMLWriter {
 		if($this->noOpenBug($pTagName)) return;
 		
 		$this->startElement($pTagName);
-		$this->writeAttribute("name", LpXmlWriter::getLpName($pUserName));
+		$this->writeAttribute("name", lpXmlWriter::getLpName($pUserName));
 		$this->writeAttribute("email", $pEmail);
 		if(isset($pFullName)) {
 			$this->text($pFullName);
@@ -99,11 +108,15 @@ class LpXmlWriter extends XMLWriter {
 		$this->endElement();
 	}
 	
+	public function writePersonA($pTagName = "person", array $pPersonArray) {
+		$this->writePerson($pTagName, $pPersonArray["nick"], $pPersonArray["email"], $pPersonArray["full"]);
+	}
+	
 	public function startBug($pId) {
 		if($this->bugIsOpen) {
 			$this->debug("Starting a new bug despite the previous one not seeming to be closed; did you call endBug() first?");
 		}
-		$this->startElementNS(NULL, "bug", LpXmlWriter::$xmlNs);
+		$this->startElementNS(NULL, "bug", lpXmlWriter::$xmlNs);
 		$this->writeAttribute("id", ((isset($pId) && is_int($pId)) ? $pId : rand())); // according to the schema, id is not optional, therefore we set a random number if none is given
 		$this->bugIsOpen = true;
 	}
@@ -117,7 +130,7 @@ class LpXmlWriter extends XMLWriter {
 	}
 	
 	public function writeBoolean($pTagName, $pBoolean) {
-		$this->writeStuff($pTagName, LpXmlWriter::getBool($pBoolean));
+		$this->writeStuff($pTagName, lpXmlWriter::getBool($pBoolean));
 	}
 	
 	public function writePrivate($pIsPrivate = true) {
@@ -137,11 +150,11 @@ class LpXmlWriter extends XMLWriter {
 	}
 	
 	public function writeDateCreated($pTimestamp) { // unix timestamp
-		$this->writeStuff("datecreated", str_replace('+00:00', 'Z', gmdate('c', $pTimestamp)));
+		$this->writeStuff("datecreated", lpXmlWriter::getTimestamp($pTimestamp));
 	}
 	
 	public function writeNickname($pNickname) {
-		$this->writeStuff("nickname", LpXmlWriter::getLpName($pNickname));
+		$this->writeStuff("nickname", lpXmlWriter::getLpName($pNickname));
 	}
 	
 	public function writeTitle($pTitle) {
@@ -161,10 +174,174 @@ class LpXmlWriter extends XMLWriter {
 	}
 	
 	public function writeMilestone($pMilestone) {
-		$this->writeStuff("milestone", LpXmlWriter::getLpName($pMilestone));
+		$this->writeStuff("milestone", lpXmlWriter::getLpName($pMilestone));
+	}
+	
+	// array in the format of "url text" => "url"
+	public function writeUrls(array $pUrls) {
+		if(isset($pUrls)) {
+			$this->startElement("urls");
+			foreach($pUrls as $title => $url) {
+				$this->startElement("url");
+				$this->writeAttribute("href", $url);
+				$this->text($title);
+				$this->endElement();
+			}
+			$this->endElement();
+		} else {
+			$this->debug("writeUrls() was called without a properly set array.");
+		}
+	}
+	
+	// array of CVE-names
+	public function writeCves(array $pCves) {
+		foreach($pCves as $key => $value) {
+			$pCves[$key] = lpXmlWriter::getCveName($value);
+		}
+		
+		$this->writeStuffArray("cves", "cve", $pCves);
+	}
+	
+	// array of tags
+	public function writeTags(array $pTags) {
+		foreach($pTags as $key => $value) {
+			$pTags[$key] = lpXmlWriter::getLpName($value);
+		}
+		
+		$this->writeStuffArray("tags", "tag", $pTags);
+	}
+	
+	// array of bugwatch-urls
+	public function writeBugwatches(array $pUrls) {
+		if(isset($pUrls)) {
+			$this->startElement("bugwatches");
+			foreach($pUrls as $url) {
+				$this->startElement("bugwatch");
+				$this->writeAttribute("href", $url);
+				$this->endElement();
+			}
+			$this->endElement();
+		} else {
+			$this->debug("writeBugwatches() was called without a properly set array.");
+		}
+	}
+	
+	// array of arrays with keys "nick", "email" and "full"
+	public function writeSubscriptions(array $pSubs) {
+		if(isset($pSubs)) {
+			$this->startElement("subscriptions");
+			foreach($pSubs as $sub) {
+				$this->writePersonA("subscriber", $sub);
+			}
+			$this->endElement();
+		} else {
+			$this->debug("writeBugwatches() was called without a properly set array.");
+		}
+	}
+	
+	/*
+		array of arrays; inner arrays contain keys:
+			- sender
+				array, keys:
+					- nick
+					- email
+					- full
+			- date (unix timestamp)
+			- title (optional)
+			- text
+			- attachments (array of attachment-arrays, optional)
+	
+	*/
+	public function writeComments(array $pComments) {
+		if(!isset($pComments)) {
+			$this->debug("Comment array was not set.");
+			return;
+		}
+		
+		$commentCount = count($pComments);
+		for($i=0; $i < $commentCount; ++$i) {
+			$p = $pComments[$i];
+			
+			$this->startElement("comment");
+			
+			$this->writePersonA("sender", $p["sender"]);
+			$this->writeStuff("date", lpXmlWriter::getTimestamp($p["date"]));
+			if(isset($p["title"])) {
+				$this->writeStuff("title", $p["title"]);
+			}
+			$this->writeStuff("text", $p["text"]);
+			
+			if(isset($p["attachments"])) {
+				$this->writeAttachments($p["attachments"]);
+			}
+			
+			$this->endElement();
+		}
 	}
 	
 	
+	/* array of attachment arrays; inner arrays contain keys:
+			mandatory:
+		- contents (raw file contents, function takes care of encoding)
+		
+			optional:
+		- url
+		- patch (boolean)
+		- filename
+		- title
+		- mimetype
+	*/
+	public function writeAttachments(array $pAttachments) {
+		if(!isset($pAttachments)) {
+			$this->debug("Attachment array was not set.");
+			return;
+		}
+		
+		$attachmentCount = count($pAttachments);
+		for($i=0; $i < $attachmentCount; ++$i) {
+			$a = $pAttachments[$i];
+			
+			$this->startElement("attachment");
+			if(isset($a["url"])) {
+				$this->writeAttribute("href", $a["url"]);
+			}
+			if(isset($a["patch"])) {
+				$this->writeStuff("type", ($a["patch"] ? lpXmlWriter::$PATCH : lpXmlWriter::$UNSPECIFIED));
+			}
+			if(isset($a["filename"]) && (strlen($a["filename"]) > 0)) {
+				$this->writeStuff("filename", $a["filename"]);
+			}
+			
+			if(isset($a["title"])) {
+				$this->writeTitle($a["title"]);
+			}
+			
+			if(isset($a["mimetype"])) {
+				$this->writeStuff("mimetype", $a["mimetype"]);
+			}
+			
+			if(isset($a["contents"]) && (strlen($a["contents"]) > 0)) {
+				$this->writeStuff("contents", base64_encode($a["contents"]));
+			} else {
+				$this->debug("Content for attachment is empty; this violates LP's schema and will not validate.");
+			}
+			
+			$this->endElement();
+		}
+	}
+	
+	
+	
+	// public helper functions
+	// returns a properly formatted array for those functions needing users like that
+	public static function getPersonArray($pUsername, $pEmail, $pFullName) {
+		return array("nick" => $pUsername, "email" => $pEmail, "full" => $pFullName);
+	}
+	public static function gpa($pUsername, $pEmail, $pFullName) {
+		return lpXmlWriter::getPersonArray($pUsername, $pEmail, $pFullName);
+	}
+	
+	// private helper functions
 	private function writeStuff($pTagName, $pStuff) {
 		if($this->noOpenBug($pTagName)) return;
 		if(!isset($pTagName)) {
@@ -178,6 +355,22 @@ class LpXmlWriter extends XMLWriter {
 		$this->writeElement($pTagName, $pStuff);
 	}
 	
+	private function writeStuffArray($pOuterTag, $pInnerTag, array $pContents) {
+		if(!isset($pOuterTag) || !isset($pInnerTag)) {
+			$this->debug("Could not output list of tags because one of the tag names was missing. Hint: $pOuterTag $pInnerTag");
+			return;
+		}
+		if(!isset($pContents)) {
+			$this->debug("write$pOuterTag() was called without a properly set array.");
+			return;
+		}
+		$this->startElement($pOuterTag);
+		foreach($pContents as $content) {
+			$this->writeStuff($pInnerTag, $content);
+		}
+		$this->endElement();
+	}
+	
 	private function noOpenBug($pTag = "tag") {
 		if(!$this->bugIsOpen) {
 			$this->debug("Could not write $pTag because no bug was open.");
@@ -189,3 +382,14 @@ class LpXmlWriter extends XMLWriter {
 		$this->writeComment("DEBUG MESSAGE: ".$pMsg);
 	}
 }
+/*
+	public function writeDescription($pDescription, $pStepsToReproduce = NULL, $pAdditionalInformation = NULL) {
+		$fullText = $pDescription;
+		if(isset($pStepsToReproduce)) {
+			$fullText .= PHP_EOL . PHP_EOL . "Steps to Reproduce:" . PHP_EOL . $pStepsToReproduce;
+		}
+		if(isset($pAdditionalInformation)) {
+			$fullText .= PHP_EOL . PHP_EOL . "Additional Information:" . PHP_EOL . $pAdditionalInformation;
+		}
+	}
+*/
