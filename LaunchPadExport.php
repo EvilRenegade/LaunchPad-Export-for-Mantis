1415ec12769fe8f87c61812806814ff4ccc9b06c
Renegade <renegade@renegadeprojects.com>
2011-11-29 01:57:11 +0100

    Added redirect-notification for exported bugs. (This assumes an entire project was moved; selective display is not implemented.)

--- LaunchPadExport.php
+++ LaunchPadExport.php
@@ -1,4 +1,5 @@
 <?php
+require_once("lpxmlwriter.php");
 class LaunchPadExportPlugin extends MantisPlugin {
     function register() {
         $this->name = 'LaunchPad Export';    # Proper name of plugin
@@ -17,11 +18,66 @@ class LaunchPadExportPlugin extends MantisPlugin {
 	
 	function hooks() {
 		return array(
-			'EVENT_MENU_FILTER' => 'getLink'			 
+			'EVENT_MENU_FILTER' => 'getLink',
+			'EVENT_VIEW_BUG_DETAILS' => 'adjustBugReport'
 		);
 	}
 	
+    function config() {
+        return array(
+            'launchpad_project' => 'launchpad'
+        );
+    }
+	
 	function getLink() {
 		return current_user_is_administrator() ? array('<a href="'.plugin_page( 'export' ).'">LaunchPad Export</a>') : array();
+	}
+	
+	/* 	this could be done more efficiently by having the exporter generate a
+		static lookup table, but this has the advantage of needing either zero
+		configuration (if the user wants no link) or only a single configurative
+		change (if the user does want the link).
+	*/
+	function adjustBugReport($pEventId, $pBugId) {
+		$projectName = plugin_config_get( 'launchpad_project' );
+		
+		if($projectName != 'launchpad') {
+			$bugName = bug_get_field($pBugId, 'summary');
+			$lpBugName = lpXmlWriter::getLpBugName($bugName);
+			// might be overkill for one row, but we've avoided raw dumping XML everywhere else in the project, so let's not start now
+			$writer = new XMLWriter();
+			$writer->openMemory();
+			$writer->startElement("tr");
+			$writer->writeAttribute("style", "vertical-align: middle;");
+			
+			$writer->startElement("td");
+			$writer->writeAttribute("colspan", "6");
+			$writer->writeAttribute("style", "border: 3px solid Red; text-align: center; line-height: 200%;");
+			
+			$writer->startElement("strong");
+			$writer->writeAttribute("style", "text-decoration: blink; color: Red; font-size: x-large;");
+			$writer->text("→");
+			$writer->endElement();
+			
+			$writer->text(" This bug has been ");
+			$writer->writeElement("strong", "moved");
+			$writer->text("! Follow ");
+			$writer->startElement("a");
+			$writer->writeAttribute("href", sprintf("https://bugs.launchpad.net/%s/+bug/%s", $projectName, $lpBugName));
+			$writer->writeAttribute("style", "font-weight: bold;");
+			$writer->text("this link");
+			$writer->endElement();
+			$writer->text(" to get to its current location. ");
+			
+			$writer->startElement("strong");
+			$writer->writeAttribute("style", "text-decoration: blink; color: Red; font-size: x-large;");
+			$writer->text("←");
+			$writer->endElement();
+			
+			$writer->endElement();
+			
+			$writer->endElement();
+			echo $writer->flush();
+		}
 	}
 }
